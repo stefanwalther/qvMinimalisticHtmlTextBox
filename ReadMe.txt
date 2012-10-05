2f60faf8249d2ef0ba3f04b997ddd9851205c8a3
Stefan Walther <stefan.walther@qlikblog.at>
2012-10-05 02:11:08 +0200

    Initial Version

---
+++ ReadMe.txt
@@ -0,0 +1,18 @@
+
+Created by Stefan Walther (http://www.qlikblog.at)
+
+Instructions:
+--------------------------------------------------
+Enter a reference to the desired QlikView field containing the Html or insert your Html as string.
+
+The following properties will be copied from the object:
+- Font Size (if not overruled by CSS)
+- Font Family (if not overruled by CSS)
+- Font Style (if not overruled by CSS)
+
+By default horizontal and vertical scrollbars will be added if necessary.
+
+
+Change Log:
+--------------------------------------------------
+1.0	09/18/2012	Initial version
---
+++ Script.js
@@ -0,0 +1,43 @@
+﻿ 
+function HtmlTextBox_Done() {
+    Qv.AddExtension("MinimalisticHtmlTextBox",
+        function() {
+             
+             var divHtml;
+             if (!this.divCreated) {
+                 divHtml = document.createElement("div");
+             	 divHtml.style.width = this.GetWidth() + "px";
+				 divHtml.style.height = this.GetHeight() + "px";
+				 divHtml.style.overflow = "auto";
+				 
+                 divHtml.innerHTML = this.Layout.Text0.text;
+				 setProps(divHtml, this);
+                 this.Element.appendChild(divHtml);
+                 this.divCreated = true;		
+             }
+             else {
+                 divHtml = this.Element.childNodes[0];
+                 divHtml.innerHTML = this.Layout.Text0.text;
+				 divHtml.style.width = this.GetWidth() + "px";
+				 divHtml.style.height = this.GetHeight() + "px";
+				 setProps(divHtml, this);
+             }
+             }, false);
+			 
+			 function setProps(obj, parentObj) {
+			 	
+				// Font Props
+				obj.style.fontStyle = parentObj.Layout.Style.fontstyle;
+				obj.style.fontFamily = parentObj.Layout.Style.fontfamily;
+				obj.style.fontSize = parentObj.Layout.Style.fontsize + "pt";
+				
+				obj.style.paddingTop = parentObj.Layout.Style.radiustopleft + "px";
+				obj.style.paddingLeft = parentObj.Layout.Style.radiustopleft + "px";
+				obj.style.paddingRight = parentObj.Layout.Style.radiustopright + "px";
+				
+				// does not make sense
+				// divHtml.style.textDecoration = this.Layout.Style.textdecoration;
+				// divHtml.style.fontColor = this.Layout.Style.fontcolor;
+			 }
+}
+HtmlTextBox_Done();
